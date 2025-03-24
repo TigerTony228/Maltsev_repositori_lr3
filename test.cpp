81bea329b98f533d9a1ba6f61c1f08d1006104c6
TigerTony228 <anton.malczew2016@yandex.ru>
2025-03-24 15:43:17 +0300

    creat task1

--- test.cpp
+++ test.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <cmath>
+#include <iomanip>
 
 using namespace std;
+
+void proverka(float& number)
+{
+    while ((!(cin >> number) || (cin.peek() != '\n')))
+    {
+        cin.clear();
+        while (cin.get() != '\n');
+        {
+            cout << "введите корректное число" << endl;
+        }
+    }
+}
 
 void proverka(int& number)
 {
@@ -14,12 +28,24 @@ void proverka(int& number)
     }
 }
 
-void task1()
+void task1(float& x)
+{
+    cout << "введите вещественное число х: ";
+    proverka(x);
+
+}
+
+void task2(int& y)
 {
 
 }
 
-void task2()
+void task3(float&x, int&y)
+{
+
+}
+
+void task4(float&x, int&y)
 {
 
 }
@@ -27,32 +53,46 @@ void task2()
 void menu()
 {
     int number;
+    float x=0;
+    int y=0;
 
     do {
         cout << "1. округлить х до у разрядов" << endl;
         cout << "2. отбросить целую часть от х" << endl;
-        cout << "3. выход" << endl;
-        cout << "введите номер задания(от 1 до 2) или 3 для выхода из программы: ";
+        cout << "3. отбросить целую часть от х" << endl;
+        cout << "4. отбросить целую часть от х" << endl;
+        cout << "5. выход" << endl;
+        cout << "введите номер задания(от 1 до 4) или 5 для выхода из программы: ";
+
+        int number;
 
         proverka(number);
 
         switch (number)
         {
         case 1:
-            task1();
+            task1(x);
             break;
 
         case 2:
-            task2();
+            task2(y);
+            break;
+        
+        case 3:
+            task3(x,y);
             break;
 
-        case 3:
+        case 4:
+            task4(x,y);
+            break;
+
+        case 5:
             cout << "выход" << endl;
             exit(0);
 
         default: menu();
         }
-    } while (number != 4);
+    } while (number != 5);
 }
 
 int main()
